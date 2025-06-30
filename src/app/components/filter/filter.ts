1b45523712d72735fc051089aaed4ae34e12aa8d
mkumar-01 <manoj.kumar@nagarro.com>
2025-06-30 11:08:13 +0530

    filter static component added

--- src/app/components/filter/filter.ts
+++ src/app/components/filter/filter.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'filter',
@@ -7,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './filter.scss'
 })
 export class Filter {
+  public selectedRange = signal<number>(8000);
+  furnished(event: Event) {
+    const input = event.target as HTMLInputElement;
+    console.log(input.checked)
+  }
+  shared(event: Event) {
+    const input = event.target as HTMLInputElement;
+    console.log(input.checked)
+  }
+  negotiable(event: Event) {
+    const input = event.target as HTMLInputElement;
+    console.log(input.checked)
+  }
+  range(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const value = Number(input.value)
+    this.selectedRange.set(value)
+  }
+
 
 }
