9b397d89a3b2a85f9e0bfcad83cc5a11c63fa3c2
mkumar-01 <manoj.kumar@nagarro.com>
2025-07-01 18:13:12 +0530

    data mentioned in dialog

--- src/app/components/dialog/dialog.component.ts
+++ src/app/components/dialog/dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, output } from '@angular/core';
+import { Component, input, output, OnInit } from '@angular/core';
 
 @Component({
   selector: 'dialog',
@@ -6,9 +6,10 @@ import { Component, output } from '@angular/core';
   templateUrl: './dialog.component.html',
   styleUrl: './dialog.component.scss'
 })
-export class DialogComponent {
+export class DialogComponent implements OnInit {
   onClose = output<boolean>();
   onPreviewSubmit = output<boolean>();
+  postData = input<any>();
   close() {
     this.onClose.emit(false)
   }
@@ -16,5 +17,9 @@ export class DialogComponent {
     this.onPreviewSubmit.emit(true)
     this.onClose.emit(false)
   }
+  ngOnInit() {
+    console.log(this.postData())
+  }
+
 
 }
